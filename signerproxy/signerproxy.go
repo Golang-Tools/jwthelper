b88824c7a7283b6ee2582869caa8648136737c75
HUANG SIZHE <hsz1273327@gmail.com>
2022-06-21 14:04:27 +0800

    Update signerproxy.go

--- signerproxy/signerproxy.go
+++ signerproxy/signerproxy.go
@@ -8,7 +8,7 @@ import (
 
 var logger *log.Log
 
-var Default *signerProxy
+var Default *SignerProxy
 
 func init() {
 	log.Set(log.WithExtFields(log.Dict{"module": "jwtsigner-proxy"}))
@@ -20,27 +20,27 @@ func init() {
 //SignerCallback 签名器操作的回调函数
 type SignerCallback func(cli jwthelper.UniversalJwtSigner) error
 
-//signerProxy 签名器的代理
-type signerProxy struct {
+//SignerProxy 签名器的代理
+type SignerProxy struct {
 	jwthelper.UniversalJwtSigner
 	opts      Options
 	callBacks []SignerCallback
 }
 
 // NewSignerProxy创建一个新的签名器代理
-func NewSignerProxy() *signerProxy {
-	proxy := new(signerProxy)
+func NewSignerProxy() *SignerProxy {
+	proxy := new(SignerProxy)
 	proxy.opts = DefaultOptions
 	return proxy
 }
 
 // IsOk 检查代理是否已经可用
-func (proxy *signerProxy) IsOk() bool {
+func (proxy *SignerProxy) IsOk() bool {
 	return proxy.UniversalJwtSigner != nil
 }
 
 //Init 条件初始化代理对象
-func (proxy *signerProxy) Init(signer jwthelper.UniversalJwtSigner, opts ...optparams.Option[Options]) error {
+func (proxy *SignerProxy) Init(signer jwthelper.UniversalJwtSigner, opts ...optparams.Option[Options]) error {
 	if proxy.IsOk() {
 		return ErrProxyAllreadySettedUniversalObject
 	}
@@ -72,7 +72,7 @@ func (proxy *signerProxy) Init(signer jwthelper.UniversalJwtSigner, opts ...optp
 
 // Regist 注册回调函数,在init执行后执行回调函数
 //如果对象已经设置了被代理客户端则无法再注册回调函数
-func (proxy *signerProxy) Regist(cb SignerCallback) error {
+func (proxy *SignerProxy) Regist(cb SignerCallback) error {
 	if proxy.IsOk() {
 		return ErrProxyAllreadySettedUniversalObject
 	}
